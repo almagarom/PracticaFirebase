4582f78056e91af0459a43eb64c7e9bf1045c06b
almagarom <87401091+almagarom@users.noreply.github.com>
2023-02-08 19:23:22 -0600

    Add files via upload
    Add correct files

--- practice.js
+++ practice.js
@@ -1,11 +1,4 @@
 
-function usuario()
-{
-  user_name = document.getElementById("user_name").value;
-  firebase.database().ref("/").child(user_name).update({
-    purpose : "adding user"
-  });
-}
 // AÑADE LOS ENLACES FIREBASE
 
 //despues de agregar una aplicación
@@ -13,14 +6,53 @@ function usuario()
 //tambien inicializa Firebase
 
 // Import the functions you need from the SDKs you need
-
+/*
 //import { initializeApp } from "firebase/app";
-//import { } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-firebase-firestore.js'
+import { } from 'https://www.gstatic.com/firebasejs/9.8.1/firebase-firestore.js'
+import { initializeApp } from '/firebase/app';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC8SSn971aICQQvmV6bb-0AWP168w9UNtM",
+  authDomain: "kwitter-ee2c1.firebaseapp.com",
+  databaseURL: 'https://kwitter-ee2c1-default-rtdb.firebaseio.com'  ,
+  projectId: "kwitter-ee2c1",
+  storageBucket: "kwitter-ee2c1.appspot.com",
+  messagingSenderId: "266981161543",
+  appId: "1:266981161543:web:fbd68126f35e0f2bbccf62"
+};
+
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+*/
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBYRLt1Ka7PXm4AF1P4fEOU36i2FwkwDmE",
+  authDomain: "vyhjbn.firebaseapp.com",
+  databaseURL: "https://vyhjbn-default-rtdb.firebaseio.com/",
+  projectId: "vyhjbn",
+  storageBucket: "vyhjbn.appspot.com",
+  messagingSenderId: "1017494382565",
+  appId: "1:1017494382565:web:765324f6e2a4f02e85f10c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function addUser()
+{
+  user_name = document.getElementById("user_name").value;
+  firebase.database().ref("/").child(user_name).update({
+    purpose : "adding user"
+  });
+}
+/*
 var firebaseConfig = {
   apiKey: "AIzaSyBCsbVncdVflv7b22vasQBlqXa2LQICHPs",
   authDomain: "kwitter123-68b8d.firebaseapp.com",
@@ -34,7 +66,4 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-
-
-
-
+*/
